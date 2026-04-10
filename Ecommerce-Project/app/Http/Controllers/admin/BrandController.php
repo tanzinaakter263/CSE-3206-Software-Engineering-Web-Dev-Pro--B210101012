7f3d719edba04a0907e0dc2115a483049856f8c6
Tanzina Akter <mahtabtanbir1@gmail.com>
2026-04-10 06:46:18 -0700

    updated Ecommerce Project and frontend

---
+++ Ecommerce-Project/app/Http/Controllers/admin/BrandController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Brand;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+class BrandController extends Controller
+{
+    public function index(){
+        $brands=Brand::orderBy('created_at','DESC')->get();
+        return response()->json([
+            'status'=>200,
+            'data'=>$brands
+        ]);
+
+    }
+    //This method will store a brand in db 
+    public function store(Request $request){
+        $validator= Validator::make($request->all(),[
+            'name' => 'required'
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'status'=>400,
+                'errors'=>$validator->errors()
+            ],400);
+        }
+        $brand=new Brand();
+        $brand->name=$request->name;
+        $brand->status=$request->status;
+        
+        $brand->save();
+        return response()->json([
+                'status'=>200,
+                'message'=>'Brand added successfully',
+                'data'=>$brand
+            ],200);
+
+
+    }
+    //This method will return a single brands
+    public function show($id){
+         $brand=Brand::find($id);
+
+         if($brand==null){
+        return response()->json([
+                'status'=>404,
+                'message'=>'Brand not found',
+                'data'=>[]
+            ],404);
+         }
+        return response()->json([
+            'status'=>200,
+            'data'=>$brand
+        ]);
+
+    }
+    //This method will update a single brand
+    public function update($id,Request $request){
+        $brand=Brand::find($id);
+
+         if($brand==null){
+        return response()->json([
+                'status'=>404,
+                'message'=>'Brand not found',
+                'data'=>[]
+            ],404);
+         }
+        $validator= Validator::make($request->all(),[
+            'name' => 'required'
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'status'=>400,
+                'errors'=>$validator->errors()
+            ],400);
+        }
+        
+        $brand->name=$request->name;
+        $brand->status=$request->status;
+        
+        $brand->save();
+        return response()->json([
+                'status'=>200,
+                'message'=>'Brand updated successfully',
+                'data'=>$brand
+            ],200);
+
+
+    }
+        
+    
+    //This method will destroy a single categories
+    public function destroy($id){
+        $brand=Brand::find($id);
+
+         if($brand==null){
+        return response()->json([
+                'status'=>404,
+                'message'=>'Brand not found',
+                'data'=>[]
+            ],404);
+         }
+         $brand->delete();
+        return response()->json([
+                'status'=>200,
+                'message'=>'Brand deleted successfully',
+            ],200);
+    }
+
+
+}
--- frontend/src/components/admin/Dashboard.jsx
+++ frontend/src/components/admin/Dashboard.jsx
@@ -1,19 +1,72 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import Layout from '../common/Layout'
-import { AdminAuthContext } from '../context/AdminAuth'
+import Sidebar from '../common/Sidebar'
+
 
 const Dashboard = () => {
-    const {logout}=useContext(AdminAuthContext);
-  return (
-    <Layout>
-        <h1>
-            Dashboard
-        </h1>
-        <button className='btn btn-danger' onClick={logout}>
-            Logout
-        </button>
-    </Layout>
-  )
+
+    return (
+        <Layout>
+            <div className='container'>
+                <div className='row'>
+                    <div className="d-flex justify-content-between mt-5 pb-3">
+                        <h4 className="h4 pb-0 mb-0"> Dashboard</h4>
+
+                    </div>
+                    <div className='col-md-3'>
+                        <Sidebar />
+
+                    </div>
+                    <div className='col-md-9'>
+                        <div className='row'>
+                            <div className='col-md-4'>
+                                <div className='card shadow'>
+                            <div className='card-body'>
+                                <h2>0</h2>
+                                <span>Users</span>
+                            </div>
+                            <div className='card-footer'>
+                                <a href="#">Views Users</a>
+                            </div>
+
+                        </div>
+
+                            </div>
+                            <div className='col-md-4'>
+                                <div className='card shadow'>
+                            <div className='card-body'>
+                                <h2>0</h2>
+                                <span>Orders</span>
+                            </div>
+                            <div className='card-footer'>
+                                <a href="#">Views Orders</a>
+                            </div>
+
+                        </div>
+
+                            </div>
+                            <div className='col-md-4'>
+                                <div className='card shadow'>
+                            <div className='card-body'>
+                                <h2>0</h2>
+                                <span>Products</span>
+                            </div>
+                            <div className='card-footer'>
+                                <a href="#">Views Products</a>
+                            </div>
+
+                        </div>
+
+                            </div>
+
+                        </div>
+                        
+                    </div>
+                </div>
+
+            </div>
+        </Layout>
+    )
 }
 
 export default Dashboard
